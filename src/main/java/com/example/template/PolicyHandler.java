ba2c00a8d95ac20da007af864bb97e9573a5b18c
user15 <user15@naver.com>
2020-06-16 07:20:32 +0900

    Modified

--- src/main/java/com/example/template/PolicyHandler.java
+++ src/main/java/com/example/template/PolicyHandler.java
@@ -12,7 +12,7 @@ import java.util.List;
 
 @Service
 public class PolicyHandler {
-    private DeliveryRepository deliveryRepository;
+   private DeliveryRepository deliveryRepository;
 
     @Autowired
     public PolicyHandler(DeliveryRepository deliveryRepository) {
@@ -21,16 +21,22 @@ public class PolicyHandler {
 
     @StreamListener(KafkaProcessor.INPUT)
     public void onListener(@Payload String message) {
+        System.out.println("##### listener : " + message);
+
         ObjectMapper objectMapper = new ObjectMapper();
         objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
 
         OrderPlaced orderPlaced = null;
         try {
             orderPlaced = objectMapper.readValue(message, OrderPlaced.class);
+
+            System.out.println(" #### type = " + orderPlaced.getEventType());
+
             /**
              * 주문이 들어옴 -> 배송 시작 이벤트 발송
              */
-            if( orderPlaced.isMe()){
+            if( orderPlaced.getEventType() != null && orderPlaced.getEventType().equals(OrderPlaced.class.getSimpleName())){
+
                 Delivery delivery = new Delivery();
                 delivery.setOrderId(orderPlaced.getOrderId());
                 delivery.setQuantity(orderPlaced.getQuantity());
@@ -45,11 +51,13 @@ public class PolicyHandler {
             /**
              * 배송이 시작됨 -> 배송 완료 이벤트 발송
              */
-            }else if(new DeliveryStarted().isMe()){
+            }else if( orderPlaced.getEventType() != null && orderPlaced.getEventType().equals(DeliveryStarted.class.getSimpleName())){
+
                 DeliveryStarted deliveryStarted = objectMapper.readValue(message, DeliveryStarted.class);
+
                 Delivery delivery = deliveryRepository.findById(deliveryStarted.getDeliveryId()).get();
                 // TODO
-                // 메모리 DB 이고, 이벤트를 처음부터 무조건 받기 때문에, 생기는 문제로, 이미 취소된상태면 완료 이벤트를 발행하지 않는다.
+                // 메모리 DB 이고, 이벤트를 처음부터 무조건 받기 때문에, 생기는 문제로, 이미 취소된상태면 완료 이벤트를 발행하지 않는다
                 if( !DeliveryCancelled.class.getSimpleName().equals(delivery.getDeliveryState())) {
                     delivery.setDeliveryState(DeliveryCompleted.class.getSimpleName());
                     deliveryRepository.save(delivery);
@@ -60,11 +68,11 @@ public class PolicyHandler {
             /**
              * 주문이 취소됨 -> 배송 취소 이벤트 발송
              */
-            }else if( new OrderCancelled().isMe() ){
-                List<Delivery> deliveries = deliveryRepository.findByOrderIdOrderByDeliveryIdDesc(orderPlaced.getOrderId());
+            }else if( orderPlaced.getEventType().equals(OrderCancelled.class.getSimpleName())){
+                List<Delivery> deliverys = deliveryRepository.findByOrderIdOrderByDeliveryIdDesc(orderPlaced.getOrderId());
 
-                if( deliveries != null && deliveries.size() > 0 ){
-                    Delivery delivery = deliveries.get(0);
+                if( deliverys != null && deliverys.size() > 0 ){
+                    Delivery delivery = deliverys.get(0);
                     delivery.setDeliveryState(DeliveryCancelled.class.getSimpleName());
                     deliveryRepository.save(delivery);
 
